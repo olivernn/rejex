851319d8ca35228d9a148a41ce92ddb6db9d60c6
olivernn <oliver.nightingale2@ntlworld.com>
2010-05-23 15:21:43 +0100

    cache the html and the stylesheets for one week

--- rejex.rb
+++ rejex.rb
@@ -4,10 +4,14 @@ require 'haml'
 
 set :haml, { :format => :html5 }
 
+# caching for one week
+
 get '/' do
+  cache_control :public => true, :max_age => 604800
   haml :index
 end
 
 get '/main.css' do
+  cache_control :public => true, :max_age => 604800
   sass :main
 end
