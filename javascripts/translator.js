b73aa9dc813212bc4dadd6da208fe026b9cee3f0
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-06-07 19:46:47 +0100

    add a few more strings to the translator

--- javascripts/translator.js
+++ javascripts/translator.js
@@ -6,7 +6,10 @@ Translator = function () {
       expression_label: 'Your Regular Expression',
       options_label: 'Options',
       test_string_label: 'Your Test String',
-      matches_label: 'Match Result'
+      matches_label: 'Match Result',
+      case_insensitive_matching: 'case insensitive matching',
+      global_matching: 'global matching',
+      multiline_matching: 'multiline matching'
     }
   }
 }
@@ -16,7 +19,7 @@ Translator.prototype = {
   detectLanguage: function () {
     // won't work in IE yet
     // this.language = window.navigator.language.substr(0, 2)
-    this.language = 'es'
+    this.language = 'en'
   },
 
   displayTranslation: function () {
