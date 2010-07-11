14ca90294088e9bd4f733fcf923a51fa14c90c0a
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-07-11 17:02:10 +0100

    generate cache manifest

--- rejex.rb
+++ rejex.rb
@@ -1,17 +1,30 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'erb'
+require 'grit'
+
+include Grit
 
 set :haml, { :format => :html5 }
 
-# caching for one week
+REPO_PATH = Sinatra::Application.root
+
+# caching for one month
 
 get '/' do
-  cache_control :public => true, :max_age => 604800
+  cache_control :public => true, :max_age => 2629743
   haml :index
 end
 
 get '/main.css' do
-  cache_control :public => true, :max_age => 604800
+  cache_control :public => true, :max_age => 2629743
   sass :main
+end
+
+get '/cache.manifest' do
+  cache_control :public => true, :max_age => 2629743
+  content_type 'text/cache-manifest'
+  @version = Repo.new(REPO_PATH).commits.first
+  erb :cache_manifest
 end
