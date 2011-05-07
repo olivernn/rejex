c4f4ac0d2fc62cd27a182ea8ccc2bf9971dfc5d4
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-05-07 19:08:24 +0100

    update the bundle and cache manifest

--- javascripts/src/rejex.js
+++ javascripts/src/rejex.js
@@ -3,7 +3,9 @@
  *
  *  Released under MIT license.
  */
+//= require "vendor/augment-0.1.0.min.js"
+//= require "vendor/jquery-1.6.min.js"
+//= require "lib/extensions.js"
 //= require "lib/jquery.inline_labels.js"
 //= require "pattern_matcher.js"
-//= require "translator.js"
 //= require "app.js"
